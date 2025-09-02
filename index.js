a857981ae240104490e8bdb2233abb73b20b64d2
cry20255-glitch <cry20255@gmail.com>
2025-09-02 08:38:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Bot is running!'));
+app.listen(3000, () => console.log('Web server running'));
 const fs = require('fs');
 const path = require('path');
 const { Client, GatewayIntentBits, Collection } = require('discord.js');
